03bda62931fbd94ccfb2329461f97e1897ade2d5
ViviErlina <h1101231010@student.untan.ac.id>
2024-09-11 14:26:11 +0700

    Praktikum 5A - Memisahkan Class

---
+++ Author.php
@@ -0,0 +1,25 @@
+class Author
+{
+    public $name;
+    public $description;
+
+    public function __construct($name, $description)
+    {
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    public function show($type): array
+    {
+        if ($type == 'short') {
+            return ['name' => $this->name];
+        } elseif ($type == 'full') {
+            return [
+                'name' => $this->name,
+                'description' => $this->description
+            ];
+        } else {
+            return [];
+        }
+    }
+}
